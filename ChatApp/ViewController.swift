ed1a5f77ba75d239562450bf0663330918a1b18b
Venkat Kukunuru <venkatkukunuru@gmail.com>
2017-03-31 13:02:36 +0530

    Initial commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by venkat on 31/03/17.
+//  Copyright © 2017 Photon Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

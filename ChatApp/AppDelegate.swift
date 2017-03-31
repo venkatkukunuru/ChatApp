45fde685e034a237ab935e8e23e604a13d7892b4
Venkat Kukunuru <venkatkukunuru@gmail.com>
2017-03-31 14:38:56 +0530

    Removed manually added XMPP files and added using pods

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import XMPPFramework
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let presence : XMPPPresence = XMPPPresence.presence()
+        let presence : XMPPPresence = XMPPPresence()
+        print("\(presence)")
         return true
     }
 
